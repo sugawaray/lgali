193524113453edc61390708692c289003fd6bee0
Yutaka Sugawara <sugawarayg@gmail.com>
2016-08-13 10:09:54 +0900

    intr: fix a commit-mistake for the previous commit.

--- tintr.c
+++ tintr.c
@@ -1,8 +1,16 @@
 #include <intr.h>
 #include <nomagicc.h>
+#include "th.h"
 
 #define S(x)	#x
 #define A(e)	do { if (!(e)) { testerror(S(e)); } } while (0)
+#define AEQ(e, x)	\
+do {	\
+	if ((e) != (x)) {	\
+		testerror(testmsg("0x%08x != 0x%08x", (e), (x)));	\
+	}	\
+} while (0)	\
+/**/
 
 static
 void
@@ -49,6 +57,29 @@ tssel()
 	A(re.ssel == 0x10);
 }
 
+static
+void
+invalidate()
+{
+	struct Idtrec o;
+
+	memset(&o, 0xFF, sizeof o);
+	idtrsetvalid(&o, 0);
+	AEQ(0x7FFF, o.attr);
+}
+
+static
+void
+validate()
+{
+	struct Idtrec o;
+
+	memset(&o, 0xAA, sizeof o);
+	o.attr &= 0x7FFF;
+	idtrsetvalid(&o, 1);
+	AEQ(0xAAAA, o.attr);
+}
+
 int
 main()
 {
@@ -56,4 +87,7 @@ main()
 	testrun("trap gate", tattrtrap);
 	testrun("interrupt gate", tattrintr);
 	testrun("segment selector", tssel);
+	testrun("invalidate", invalidate);
+	testrun("validate", validate);
+	return 0;
 }
