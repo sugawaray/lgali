4538158d41d223f95292401dcd3d2aa3386b6ae1
Yutaka Sugawara <sugawarayg@gmail.com>
2016-08-13 14:38:29 +0900

    all: added a new bin to experiment/study LAPIC timer.

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 .POSIX:
 .SUFFIXES: .o .to .s
 
-TGTOBJS = eintr.o eled.o emc.o eser.o eeth.o eioapic.o
+TGTOBJS = eintr.o eled.o emc.o eser.o eeth.o eioapic.o etm.o
 TESTENTS = tintr.to tethim.to tmii.to tethim_read.to
 TARGETS = $(TGTOBJS:.o=)
 TESTTGTS = $(TESTENTS:.to=)
@@ -9,7 +9,7 @@ OBJS = intr.o lib.o mc.o quarkim.o ser.o serpri.o quark.o pcic.o led.o ethim.o m
 HOBJS = serout.o
 TOBJS = serstub.to $(OBJS:.o=.to)
 
-all: $(TARGETS) $(TESTTGTS)
+all: $(TARGETS) $(TESTTGTS) calltest
 
 clean:
 	-rm $(TARGETS) $(TGTOBJS) $(OBJS) $(HOBJS) $(TESTTGTS) $(TESTENTS) $(TOBJS)
@@ -39,6 +39,7 @@ tmii.to: tmii.c miiim.h
 ioapic.o: ioapic.c include/ioapic.h
 ethim.o: ethim.c ethim.h include/eth.h
 ethim.to: ethim.c ethim.h include/eth.h
+etm.o: etm.c
 eeth.o: eeth.c ethim.h include/eth.h
 hstbr.o: hstbr.c include/quark.h include/hstbr.h
 serout.o: serout.c include/ser.h serim.h
---
+++ etm.c
@@ -0,0 +1,67 @@
+#include "intrim.h"
+#include <intr.h>
+#include <led.h>
+#include <quark.h>
+#include <ser.h>
+
+enum {
+	Irqstimer	= 0x20
+};
+
+static struct Idtrec idt[0x100];
+
+static
+void
+initintr()
+{
+	char tp[6];
+	struct Idtrec *rec;
+
+	sidt(tp);
+	mmemcpy(idt, *(u32 *)(tp + 2), *(u16 *)tp + 1);
+	rec = &idt[Irqstimer];
+	idtrsetssel(rec, 0x02);
+	idtrsettype(rec, IDTRINTR);
+	idtrsetad(rec, ontimer);
+
+	cli();
+	ic8259_init(Irqstimer);
+	/* initialize i8259 */
+	/*
+	ic8259setmsk(mmsk, smsk);
+	*/
+	packt((const char *)idt, sizeof idt / sizeof idt[0], tp);
+	lidt(tp);
+	sti();
+}
+
+static
+void
+timerinit()
+{
+	outb(0x43, 0x36);
+	outb(0x40, 0xFF);
+	outb(0x40, 0xFF);
+}
+
+int
+main()
+{
+	int r;
+
+	wait();
+	ledminit();
+	r = serinit();
+	if (r != 0)
+		ledmemit(5, 100);
+	else {
+		initintr();
+		timerinit();
+		/* initialize i8254 */
+		/* initialize LAPIC timer */
+		/* stop i8254 system timer */
+	}
+	for (;;)
+		;
+	return 0;
+}
